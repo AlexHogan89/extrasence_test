5e2ca005045fd4bce609c29dd1fd84ae54491824
AlexHogan89 <95272174+AlexHogan89@users.noreply.github.com>
2021-11-30 22:54:24 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,40 +1,17 @@
 <?
+    //ini_set('error_reporting', E_ALL);
+    //ini_set('display_errors', 1);
+    //ini_set('display_startup_errors', 1);
+    
     session_start();
-?>
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  
-  <h3>Тестирование Экстрасенсов</h3>
-
-<h4>Загадайте число от 0 до 10 и узнайте какой экстрасенс лучше</h4>
-
-<form method="POST">
-    <input maxlength="3" name="NumInput" size="3" type="number" />
-	<input type="submit" name="SendButton" value="Отправить экстрасенсам"/>
-</form>
-
-<br>
-<?
+    
     class ExtraSense {
-        public $name;
-        public $truth;
+        public $name = '';
+        public $truth = 0;
+        public $guess = 0;
 
         function set_name($name) {
         $this->name = $name;
-        }
-        
-        function set_truth($truth) {
-        $this->truth = $truth;
-        }
-        
-        function get_name() {
-        return $this->name;
-        }
-        
-        function get_truth() {
-        return $this->truth;
         }
     }
     
@@ -47,24 +24,62 @@
     $extrasense3->set_name('Магистр Тьмы');
    
     $number = 0;
-    $full = array(array());
-    $current = array();
-    //$truth_of_extra1 = 0;
-    //$truth_of_extra2 = 0;
-    //$truth_of_extra3 = 0;
-    $counter = 0;
+    if(isset($_POST["NumInput"])){
+        $number = $_POST["NumInput"];
+    }
     
+    $NumHistory = array(array());
+    $current = array();
+    
+    $TruthData = array(0,0,0);
+    if(isset($_SESSION['truth_data'])){
+        $TruthData = $_SESSION['truth_data'];
+        $extrasense1->truth = $TruthData[0];
+        $extrasense2->truth = $TruthData[1];
+        $extrasense3->truth = $TruthData[2];
+    }
+    
+    $GuessCounter = 0;
+    if (isset($_SESSION['guess_counter']))
+        {
+            $GuessCounter = $_SESSION['guess_counter'];
+        }
+    else
+    {
+        $_SESSION['guess_counter'] = $GuessCounter;
+    }
+        
+    $step = 0; // флаг шага: 0 - предложение загадать, 1 - вывод догадок
+    if (isset($_SESSION['step']))
+        {
+            $step = $_SESSION['step'];
+        }
+    if (isset($_REQUEST['GuessButton'])){
+        $_SESSION['step'] = $step++;    
+    }
+
+
     function PercentOfLuck(ExtraSense $t){
-    	global $counter;
-        $d = round($t->truth*100/$counter, 1);;
+    	global $GuessCounter;
+        $d = round($t->truth*100/$GuessCounter, 1);;
         return $d;
     }
-   
-    function Debug() {
-        echo $truth_of_extra1 . '<br>' . $truth_of_extra2 . '<br>' . $truth_of_extra3 . '<br>' . $counter . '<br>'; //вывод счетчиков
+
+    function DrawGuessTable() {
+        global $extrasense1;
+        global $extrasense2;
+        global $extrasense3;
+        
+        echo '<br>';
+        echo "Догадки:";
+        echo '<table border="1" cellpadding=10>';
+        echo '<tr>'. '<td>'. $extrasense1->name . '</td>' . '<td>' . $extrasense1->guess  . '</td>' . '</tr>';
+        echo '<tr>'. '<td>'. $extrasense2->name . '</td>' . '<td>' . $extrasense2->guess  . '</td>' . '</tr>';
+        echo '<tr>'. '<td>'. $extrasense3->name . '</td>' . '<td>' . $extrasense3->guess  . '</td>' . '</tr>';
+        echo '</table><br>';
     }
-    
-    function DrawTrouthTable() {
+
+    function DrawTruthTable() {
         global $extrasense1;
         global $extrasense2;
         global $extrasense3;
@@ -74,7 +89,7 @@
         echo '<tr>'. '<td>'. $extrasense1->name . '</td>' . '<td>' . PercentOfLuck($extrasense1)  . "%" . '</td>' . '</tr>';
         echo '<tr>'. '<td>'. $extrasense2->name . '</td>' . '<td>' . PercentOfLuck($extrasense2)  . "%" . '</td>' . '</tr>';
         echo '<tr>'. '<td>'. $extrasense3->name . '</td>' . '<td>' . PercentOfLuck($extrasense3)  . "%" . '</td>' . '</tr>';
-        echo '</table>';
+        echo '</table><br>';
     }
  
     function DrawHistoryTable() {
@@ -88,108 +103,72 @@
         echo '<tr>'. '<td>'. "Загаданное число" . '</td>' . '<td>'. $extrasense1->name . '</td>' . '<td>'. $extrasense2->name . '</td>' . '<td>'. $extrasense3->name . '</td>' . '</tr>';
         
         
-        foreach ($_SESSION['full'] as $item)
+        foreach ($_SESSION['NumHistory'] as $item)
         {
     	    echo '<tr>'. '<td>'. $item[0]. '</td>' . '<td>'. $item[1]. '</td>' . '<td>'. $item[2]. '</td>' . '<td>'. $item[3] . '</td>' . '</tr>';
         }
         
-        echo '</table>';
+        echo '</table><br>';
     }
-    
-    if (isset($_SESSION['counter']))
-        {
-            $counter = $_SESSION['counter'];
-        }
-    else
-        {
-             $counter = 0;
-             $_SESSION['counter'] = $counter;
-        }
-    
-    if(isset($_POST["NumInput"])){
-        $number = $_POST["NumInput"];
-    }
-    
-    if (isset($_REQUEST['SendButton']))
-    {
-        $extra1 = rand(0,10);
-        $extra2 = rand(0,10);
-        $extra3 = rand(0,10);
-        
-       
-        $counter++;
-        $_SESSION['counter'] = $counter;
-        
-        if (isset($_SESSION['truth_of_extra1']))
-        {
-            $extrasense1->truth = $_SESSION['truth_of_extra1'];
-        }
-        else
-        {
-            $extrasense1->truth = 0;
-            $_SESSION['truth_of_extra1'] = $extrasense1->truth;
-        }
-        
-        if (isset($_SESSION['truth_of_extra2']))
-        {
-            $extrasense2->truth = $_SESSION['truth_of_extra2'];
-        }
-        else
-        {
-            $extrasense2->truth = 0;
-            $_SESSION['truth_of_extra2'] = $extrasense2->truth;
-        }
-        
-        if (isset($_SESSION['truth_of_extra3']))
-        {
-            $extrasense3->truth = $_SESSION['truth_of_extra3'];
-        }
-        else
-        {
-            $extrasense3->truth = 0;
-            $_SESSION['truth_of_extra3'] = $extrasense3->truth;
-        }
-        
-        $full = $_SESSION['full'];
-        
-        if($number == $extra1) $extrasense1->truth++;
-        if($number == $extra2) $extrasense2->truth++;
-        if($number == $extra3) $extrasense3->truth++;
-        
-        $_SESSION['truth_of_extra1'] = $extrasense1->truth;
-        $_SESSION['truth_of_extra2'] = $extrasense2->truth;
-        $_SESSION['truth_of_extra3'] = $extrasense3->truth;
-        
-        $current = [$number, $extra1, $extra2, $extra3];
-        $full[] = $current;
-        
-        $_SESSION['full'] = $full;
-        
-        //Debug();
-        if($counter > 0) {
-            DrawTrouthTable();
-        }
-        
-        echo '<br>';
-        DrawHistoryTable();
-    /*    echo "История:";
-        echo '<table border="1" style="text-align:center;" cellpadding=10>';
-        
-        echo '<tr>'. '<td>'. "Загаданное число" . '</td>' . '<td>'. "Экстрасенс Влад" . '</td>' . '<td>'. "Экстрасенс Олег" . '</td>' . '<td>'. "Экстрасенс Афдотья" . '</td>' . '</tr>';
-        
-        
-        foreach ($_SESSION['full'] as $item)
-        {
-    	    echo '<tr>'. '<td>'. $item[0]. '</td>' . '<td>'. $item[1]. '</td>' . '<td>'. $item[2]. '</td>' . '<td>'. $item[3] . '</td>' . '</tr>';
-        }
-        
-        echo '</table>'; */
-
-    }
-    
-    
-    
 ?>
-
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <h3>Тестирование Экстрасенсов</h3>
+    <h4>Загадайте число и узнайте какой экстрасенс лучше</h4>
+    <br>
+    
+<?
+ if($step == 0){
+    echo '<h4>Загадайте число от 10 до 99</h4>';
+    echo '<form method="POST"><input type="submit" name="GuessButton" value="Загадал"/></form><br>';
+    
+    if (isset($_SESSION['current'])){
+        $current = $_SESSION['current'];
+        $current[0] = $number;
+        
+        
+        if($number == $current[1]) $extrasense1->truth++;
+        if($number == $current[2]) $extrasense2->truth++;
+        if($number == $current[3]) $extrasense3->truth++;
+        
+        $TruthData[0] = $extrasense1->truth;
+        $TruthData[1] = $extrasense2->truth;
+        $TruthData[2] = $extrasense3->truth;
+        
+        $_SESSION['truth_data'] = $TruthData;
+        
+        if(isset($_SESSION['NumHistory'])){
+            $NumHistory = $_SESSION['NumHistory'];
+            $NumHistory[] = $current;
+        }
+        else
+        {
+            $NumHistory[0] = $current;
+        }
+        $_SESSION['NumHistory'] = $NumHistory;
+       // echo $GuessCounter;
+        DrawTruthTable();
+        DrawHistoryTable();
+    }
+ }
+ 
+ if($step == 1){
+    echo '<h4>Введите число которое загадали</h4>';
+    echo '<form method="POST"><input maxlength="2" name="NumInput" size="2" type="number"/>';
+    echo '<input type="submit" name="SendButton" value="Отправить"/></form><br>';
+    
+    $GuessCounter++;
+    $_SESSION['guess_counter'] = $GuessCounter;
+   // echo $GuessCounter;
+    
+    //$current = [0, $extra1, $extra2, $extra3];
+    $_SESSION['current'] = [0, $extrasense1->guess = mt_rand(10,99), $extrasense2->guess = mt_rand(10,50), $extrasense3->guess = mt_rand(10,99)];
+    
+    DrawGuessTable();
+ }
+ 
+?>
 </body>
 </html>
